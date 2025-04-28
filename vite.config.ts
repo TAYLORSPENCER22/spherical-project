dc597b1258019b0ab49ff46a8fdf2da448b3aa25
TaylorSpencer22 <taylorspencer522@gmail.com>
2025-04-28 01:22:05 -0700

    vite-config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+  base: '/spherical-project/',
+  
   plugins: [react()],
   server: {
     port:3000, //changed to local host 3000
